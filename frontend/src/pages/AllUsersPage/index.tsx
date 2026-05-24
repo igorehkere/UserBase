4a252ac07c41192fb69d24fd0b425ccb76300c66
Igorechkere <igorshvedenko@yandex.ru>
2026-05-24 18:09:22 +0300

    Add commit rules2

--- frontend/src/pages/AllUsersPage/index.tsx
+++ frontend/src/pages/AllUsersPage/index.tsx
@@ -1,29 +1,26 @@
-import { trpc } from "../../utils/trpc"
-import css from "./index.module.scss"
+import { trpc } from "../../utils/trpc";
+import css from "./index.module.scss";
 
-export function AllUsersPage () {
-    const {data, isError, isLoading, isFetching, error} = trpc.getUsers.useQuery()
-    if (isLoading || isFetching) {
+export function AllUsersPage() {
+  const { data, isError, isLoading, isFetching, error } =
+    trpc.getUsers.useQuery();
+  if (isLoading || isFetching) {
+    return <div className={css.loading}>Loading...</div>;
+  }
+  if (isError) {
+    return <div className={css.error}>Error: {error.message}</div>;
+  }
+  return (
+    <div className={css.container}>
+      <h1>All Users:</h1>
+      {data.users.map((user) => {
         return (
-            <div className={css.loading}>Loading...</div>
-        )
-    }
-    if (isError) {
-        return (
-            <div className={css.error}>Error: {error.message}</div>
-        )
-    }
-    return (
-        <div className={css.container}>
-            <h1>All Users:</h1>
-            {data.users.map((user) => {
-                return (
-                    <div className={css.card} key={user.id}>
-                        <p>nick: {user.nick}</p>
-                        <p>Name: {user.name}</p>
-                    </div>
-                )
-            })}
-        </div>
-    )
+          <div className={css.card} key={user.id}>
+            <p>nick: {user.nick}</p>
+            <p>Name: {user.name}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
