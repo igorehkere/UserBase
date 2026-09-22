8e284c4ee03c7bee113b13c34935350c1d6f5aa9
igorehkere <igorshvedenko52@gmail.com>
2026-09-22 21:39:14 +0300

    Refactoring user profile

--- frontend/src/pages/users/MyProfilePage/index.tsx
+++ frontend/src/pages/users/MyProfilePage/index.tsx
@@ -6,18 +6,22 @@ import { getData } from '../../../utils/getData';
 import { ButtonChange } from '../../../components/Button';
 import { useState } from 'react';
 import { EditPostContent } from '../../posts/EditPostPage';
-import { AnimatePresence } from "framer-motion";
-import { HiOutlinePencil } from "react-icons/hi";
+import { AnimatePresence } from 'framer-motion';
+import { HiOutlinePencil } from 'react-icons/hi';
 import { Link } from 'react-router-dom';
 import { getEditMyProfile } from '../../../lib/routes';
+import { getAvatarUrl } from '@authwithback/shared/src/cloudinary';
 
 export function MyProfilePage() {
   const me = useMe();
-  const [showModalWindow, setShowModalWindow] = useState<null | NonNullable<ReturnType<typeof useMe>>['posts'][number]>(null);
+  const [showModalWindow, setShowModalWindow] = useState<null | NonNullable<ReturnType<typeof useMe>>['posts'][number]>(
+    null
+  );
 
   function getPostForModal(post: NonNullable<ReturnType<typeof useMe>>['posts'][number] | null) {
-    setShowModalWindow(post)
+    setShowModalWindow(post);
   }
+
   return (
     <>
       <Helmet>
@@ -30,37 +34,49 @@ export function MyProfilePage() {
         ) : (
           <>
             <AnimatePresence>
-              {showModalWindow ? <EditPostContent getPostForModal={getPostForModal} post={showModalWindow}/> : null}
+              {showModalWindow ? <EditPostContent getPostForModal={getPostForModal} post={showModalWindow} /> : null}
             </AnimatePresence>
-            <h1>Профиль</h1>
-            <div className={css.card1}>
-              <div>
-                <p>{`ФИО - ${me.firstname} ${me.lastname}`}</p>
-                <p>Ник - {me.nick}</p>
-              </div>
-              <Link to={getEditMyProfile()}>
-                <HiOutlinePencil className={css.pencil}/>
-              </Link>
-              
-            </div>
-            <h1>Ваши посты</h1>
-            {me.posts.map((post) => {
-              const date = getData(post.createdAt);
-              if (post.blockedAt) {
-                return null
-              }
-              return (
-                <div className={css.card2} key={post.id}>
-                  <p>{post.text}</p>
-                  <div className={css.panel}>
-                    <ButtonChange onClick={() => {
-                      getPostForModal(post)
-                    }}>Изменить</ButtonChange>
-                    <span>{date}</span>
+            <div className={css.newProfile}>
+              <div className={css.infoAboutYou}>
+                <div>
+                  <img alt="" className={css.avatar} src={getAvatarUrl(me.avatar, 'small')} />
+                </div>
+                <Link to={getEditMyProfile()} className={css.pencilWr}>
+                  <HiOutlinePencil className={css.pencil} />
+                </Link>
+                <div className={css.card1}>
+                  <div className={css.data}>
+                    <p className={css.name}>{`${me.firstname} ${me.lastname}`}</p>
+                    <p className={css.nick}>@{me.nick}</p>
                   </div>
                 </div>
-              );
-            })}
+              </div>
+              <hr />
+              <h1>Ваши посты</h1>
+              <div>
+                {me.posts.map((post) => {
+                  const date = getData(post.createdAt);
+                  if (post.blockedAt) {
+                    return null;
+                  }
+                  return (
+                    <div className={css.card2} key={post.id}>
+                      <p>{post.text}</p>
+                      <div className={css.panel}>
+                        <ButtonChange
+                          onClick={() => {
+                            getPostForModal(post);
+                          }}
+                        >
+                          Изменить
+                        </ButtonChange>
+                        <span>{date}</span>
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
           </>
         )}
       </div>
